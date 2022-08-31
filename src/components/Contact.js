f465b0a1da06eda5b177a8640fc3415a82fc704f
sonukumar77 <82059136+sonukumar77@users.noreply.github.com>
2022-08-31 23:32:13 +0530

    updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,13 +4,13 @@ const Contact = _ => {
     return(
        <section>
          <h2> 📞 Contact Me</h2>
-         <a href="https://www.linkedin.com/in/sonu-kumar-1110b81a0" target="_blank">LinkedIn</a>
+         <a href="https://www.linkedin.com/in/sonu-kumar-1110b81a0" target="_blank" rel="noreferrer">LinkedIn</a>
        
-         <a href="https://www.github.com/sonukumar77" target="_blank">GitHub</a>
+         <a href="https://www.github.com/sonukumar77" target="_blank" rel="noreferrer">GitHub</a>
         
-         <a href="https://www.youtube.com" target="_blank">YouTube</a>
+         <a href="https://www.youtube.com" target="_blank" rel="noreferrer">YouTube</a>
       
-         <a href="https://www.instagram.com" target="_blank">Instagrarm</a>
+         <a href="https://www.instagram.com" target="_blank" rel="noreferrer">Instagrarm</a>
        </section>
     )
 }
